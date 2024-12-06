d95c96b3890c1d72fc7d8b1db21065a802922419
Aditi Sharma <112544669+aditi1807@users.noreply.github.com>
2024-12-06 14:02:42 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-DATABASE_URL = 'postgresql://postgres:YourPassword@localhost:5432/QuizzApplicationYT'
+DATABASE_URL = 'postgresql://postgres:YourPassword@localhost:5432/QuizzApplication'
 
 engine = create_engine(DATABASE_URL)
 
